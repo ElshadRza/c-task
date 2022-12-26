fa0afee24584c248bb6eba639b2bc3f7a1d5f880
berserk222 <118837671+berserk222@users.noreply.github.com>
2022-12-26 18:24:28 +0400

    Add files via upload

---
+++ C/Person/Models/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.Models
+{
+    public abstract class Animal
+
+    {
+        public int AnimalLeg { get; set; }
+        public bool AnimalTail { get; set; }
+        public bool AnimalHorn { get; set; }
+        public int HornCount { get; set; }
+        public Animal() { }
+        
+        
+   
+        
+    }
+
+    public sealed class Elephant : Animal
+    {
+
+        public Elephant(int AnimalLeg, int HornCount, bool AnimalTail, bool AnimalHorn) : base()
+        {
+            Console.WriteLine("This Animal has {0} legs.",AnimalLeg);
+           if (AnimalTail == true)
+            {
+                Console.WriteLine("This Animal has a tail.");
+            }
+            else
+            {
+                Console.WriteLine("This animal doesnt have a tail.");
+            }
+            if(AnimalHorn== true&&HornCount>1)
+            {
+                Console.WriteLine("This Animal has {0} horns.",HornCount);
+            }
+            else if(AnimalHorn==true&&HornCount ==1) 
+            {
+                Console.WriteLine("This Animal has a horn.");
+            }
+            else if (AnimalHorn == false)
+            {
+                Console.WriteLine("This Animal doesnt have a horn.");
+            }
+                
+        }
+
+    }
+}
---
+++ C/Person/Program.cs
@@ -0,0 +1,4 @@
+﻿using Abstract.Models;
+
+Animal MaleElephant = new Elephant(4,2,true,true);
+
